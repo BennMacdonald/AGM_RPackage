34c1b69279ceab2c8d8adfb127586a9d84e013f2
BennMacdonald <Benn.Macdonald@glasgow.ac.uk>
2017-09-07 16:39:45 +0100

    Updated, as results can now be returned as well as saved to disk

--- run_script.R
+++ run_script.R
@@ -27,13 +27,18 @@ LV_func = function(t, X, params) {
 
 # AGM
 agm(data=dataTest,time=timeTest,ode.system=LV_func,numberOfParameters=4,temperMismatchParameter=TRUE,
+   maxIterations=100000,originalSignalOnlyPositive=TRUE, showProgress=TRUE, returnResults=FALSE, noise.sd=0.1,
+   defaultPrior="Gamma",defaultTemperingScheme="LB10")
+
+agmResults <- agm(data=dataTest,time=timeTest,ode.system=LV_func,numberOfParameters=4,temperMismatchParameter=TRUE,
    maxIterations=100000,originalSignalOnlyPositive=TRUE, showProgress=TRUE, noise.sd=0.1,
    defaultPrior="Gamma",defaultTemperingScheme="LB10")
+
 
 # Explicit ODE solution
 agm(data=dataTest,time=timeTest,ode.system=LV_func,observedSpecies=1,numberOfParameters=6,temperMismatchParameter=TRUE,
     maxIterations=100000,originalSignalOnlyPositive=TRUE, 
-    showProgress=TRUE, noiseInfer=TRUE,
+    showProgress=TRUE, returnResults=FALSE, noiseInfer=TRUE,
     defaultPrior="Gamma",defaultTemperingScheme="LB10",explicit=TRUE)
 
 
